3a466e08fac2141c97c5805dd70cf701dbab3da0
prabashwara65 <prabashwara65@gmail.com>
2024-12-25 17:33:09 +0530

    added UserRegistration model

---
+++ Backend/models/UserRegistration.js
@@ -0,0 +1,10 @@
+import mongoose from 'mongoose'
+
+const UserSchema = new mongoose.Schema({
+    name : {type : String},
+    email : {type : String},
+    password : {type : String}
+})
+
+const User = mongoose.model('user' , UserSchema);
+export default User
