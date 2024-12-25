b25bc9a5274cba10b2e58a8a4ec38f1dc4210860
prabashwara65 <prabashwara65@gmail.com>
2024-12-25 15:11:08 +0530

    developing sign up page

--- Frontend/src/pages/signup.jsx
+++ Frontend/src/pages/signup.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const signup = () => {
-  return <div>signup</div>;
+  return (
+    <div>
+      <h2>Sign Up</h2>
+      <form>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input type="text" placeholder="Enter Name" />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" placeholder="Enter Email" />
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default signup;
