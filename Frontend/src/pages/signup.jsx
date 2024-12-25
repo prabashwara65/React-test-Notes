fd63f72028f3de445dd0eaaefeb8c622cd7b61f1
prabashwara65 <prabashwara65@gmail.com>
2024-12-25 15:48:33 +0530

    developed App.jsx

--- Frontend/src/pages/signup.jsx
+++ Frontend/src/pages/signup.jsx
@@ -1,21 +1,28 @@
 import React from "react";
 
-const signup = () => {
+const Signup = () => {
   return (
-    <div>
-      <h2>Sign Up</h2>
-      <form>
-        <div>
-          <label htmlFor="name">Name</label>
-          <input type="text" placeholder="Enter Name" />
-        </div>
-        <div>
-          <label htmlFor="email">Email</label>
-          <input type="text" placeholder="Enter Email" />
-        </div>
-      </form>
+    <div className="flex justify-center items-center">
+      <div>
+        <h2>Sign Up</h2>
+        <form>
+          <div>
+            <label htmlFor="name">Name</label>
+            <input type="text" placeholder="Enter Name" />
+          </div>
+          <div>
+            <label htmlFor="email">Email</label>
+            <input type="text" placeholder="Enter Email" />
+          </div>
+          <div>
+            <label htmlFor="password">Password</label>
+            <input type="text" placeholder="******" />
+          </div>
+          <button>Sign Up</button>
+        </form>
+      </div>
     </div>
   );
 };
 
-export default signup;
+export default Signup;
