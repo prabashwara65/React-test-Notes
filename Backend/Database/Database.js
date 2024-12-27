e7d927cb8ec894ff82ac2bfd9de9fa27d697f352
prabashwara65 <prabashwara65@gmail.com>
2024-12-27 11:33:52 +0530

    added database urls

--- Backend/Database/Database.js
+++ Backend/Database/Database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const connectToDatabase = async () => {
   try {
-    await mongoose.connect();
+    await mongoose.connect('mongodb+srv://prabashwara65:Rr36JRUgFVNylsuy@cluster0.qtbww.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
     console.log("Connection is successfull"); 
 
   } catch (error) {
