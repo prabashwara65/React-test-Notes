99b18df31bbee5b955644beaf505112b0ba7685a
prabashwara65 <prabashwara65@gmail.com>
2024-12-30 11:29:35 +0530

    creating home and login jsx files

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,16 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Signup from "./pages/signup";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/signup" element={<Signup />} />
+          <Route path="/login" element={<Login />} />
+
         </Routes>
       </BrowserRouter>
     </>
