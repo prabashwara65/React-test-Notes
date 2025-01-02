7642779fb892cae25ba8333503a89a3780bbde53
prabashwara65 <prabashwara65@gmail.com>
2025-01-02 15:19:49 +0530

    created contextprovider API

---
+++ Frontend/src/context/ContextProvider.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useState } from 'react'
+
+const authContext = createContext()
+
+const ContextProvider = ({ children }) => {
+    const [user , setUser] = useState(null);
+  return (
+    <authContext.Provider value={{user}}>
+        {children}
+    </authContext.Provider>
+  )
+}
+export const useAuth = useContext(authContext);
+export default ContextProvider
