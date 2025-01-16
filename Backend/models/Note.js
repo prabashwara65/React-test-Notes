d9406e3acbf6aee2ac5815eb774b29eb4175dfec
prabashwara65 <prabashwara65@gmail.com>
2025-01-16 13:11:06 +0530

    correction

--- Backend/models/Note.js
+++ Backend/models/Note.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose'
 const NoteSchema = new mongoose.Schema({
     title : {type : String},
     description : {type : String},
-    userID : {type : mongoose.Schema.Types.ObjectId},
+    userID : {type : mongoose.Schema.Types.ObjectId , ref: 'User'},
 })
 
 const Note = mongoose.model('Note' , NoteSchema);
