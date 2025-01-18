9560385850ec736422b8bf307cd6c23831382b4c
prabashwara65 <prabashwara65@gmail.com>
2025-01-18 20:52:21 +0530

    fixing note routes

--- Backend/routes/note.js
+++ Backend/routes/note.js
@@ -1,8 +1,23 @@
 import express from 'express'
+import Note from '../models/Note'
 
 const router = express.Router()
 
 router.post('/add' , async (req , res) => {
+
+    try{
+
+        const { title , description } = req.body;
+
+        const newNote = new Note ({
+            title,
+            description,
+        })
+
+
+    }catch(error){
+
+    }
 
 })
 
